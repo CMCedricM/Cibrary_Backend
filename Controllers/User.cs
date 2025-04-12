c94499fe217d06f067061fe5676b94bf2c70f766
Cedric Men <c.m.cedricm019@gmail.com>
2025-04-12 01:40:09 -0700

    Add project files.

---
+++ Controllers/User.cs
@@ -0,0 +1,32 @@
+﻿using Cibrary_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cibrary_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class User : Controller
+    {
+
+        static private readonly UserProfile testProfile = new()
+        {
+            UserName = "testUser",
+            Email = "test@email.com",
+            FirstName = "FirstName"
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult<UserProfile> GetUserInfo()
+        {
+            return Ok(testProfile);
+        }
+
+        
+       
+    }
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace Cibrary_Backend.Models
+{
+    public class UserProfile
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
+
+
+    }
+}
